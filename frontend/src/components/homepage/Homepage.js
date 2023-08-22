fa7bebd6f9003d26a0a407145f326492b944d602
fatih-celebi <celebifatih.c1@gmail.com>
2023-08-22 23:02:18 +0100

    add homepage functionalities

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,9 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Import React Router modules
 import MovieLinks from "../movielinks/MovieLinks";
-import '../homepage/Homepage.css'
+import "../homepage/Homepage.css";
 import Navbar from "../Navbar/Navbar";
-
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import CameraIcon from "@mui/icons-material/PhotoCamera";
@@ -20,120 +19,79 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 // import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { TextField } from "@mui/material";
+const defaultTheme = createTheme();
 
-
-   const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-   const defaultTheme = createTheme();
 
 const Homepage = () => {
-    // const [searchResults, setSearchResults] = useState([]);
-    // const [title, setTitle] = useState("");
-    // const [showResults, setShowResults] = useState(false)
-
-
-    // const handleSearch = async () => {
-    //     try {
-    //         const response = await fetch(`/homepage/bytitle/${title}`);
-    //         const data = await response.json();
-    //         setSearchResults(data); // data is an array, no need for []
-    //         setShowResults(true)
-    //     } catch (error) {
-    //         console.error("Error fetching search results: ", error)
-    //     }
-    // };
-
- 
-
-    return (
-      // <div className="main-homepage-div">
-      //   <h1 id="heading">Search for a movie or tv show title</h1>
-      //   <input
-      //     type="text"
-      //     placeholder="Enter a title"
-      //     value={title}
-      //     onChange={(event) => setTitle(event.target.value)}
-      //   />
-      //   <button onClick={handleSearch}>Search</button>
-      //   {showResults && (
-      //     <div>
-      //       <h2>Search Results</h2>
-      //       <div>
-      //         {searchResults.map((result, index) => (
-      //           <div key={index}>
-      //             <Link to={`/movie-links/${result.id}/${result.title}`} style={{ textDecoration: 'none' }}><h2>{result.title}</h2></Link>
-      //             <p>Synopsis: {result.overview}</p>
-      //             <div className="poster-container">
-      //               <img
-      //                 className="poster-image"
-      //                 src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`} // poster_path is null in some movies
-      //                 alt={result.title}
-      //               />
-      //             </div>
-      //             <p>Rating: {result.vote_average}</p>
-
-      //           </div>
-      //         ))}
-      //       </div>
-      //     </div>
-      //   )}
-      // </div>
-
-      <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <CameraIcon sx={{ mr: 2 }} />
-            <Typography variant="h6" color="inherit" noWrap>
-              Album layout
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          {/* Hero unit */}
-          <Box
-            sx={{
-              bgcolor: "background.paper",
-              pt: 8,
-              pb: 6,
-            }}
-          >
-            <Container maxWidth="sm">
-              <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-                gutterBottom
-              >
-                Album layout
-              </Typography>
-              <Typography
-                variant="h5"
-                align="center"
-                color="text.secondary"
-                paragraph
-              >
-                Something short and leading about the collection below—its
-                contents, the creator, etc. Make it short and sweet, but not too
-                short so folks don&apos;t simply skip over it entirely.
-              </Typography>
-              <Stack
-                sx={{ pt: 4 }}
-                direction="row"
-                spacing={2}
-                justifyContent="center"
-              >
-                <Button variant="contained">Main call to action</Button>
-                <Button variant="outlined">Secondary action</Button>
-              </Stack>
-            </Container>
-          </Box>
-          <Container sx={{ py: 8 }} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-              {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
+  const [searchResults, setSearchResults] = useState([]);
+  const [title, setTitle] = useState("");
+  const [showResults, setShowResults] = useState(false);
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`/homepage/bytitle/${title}`);
+      const data = await response.json();
+      setSearchResults(data);
+      setShowResults(true);
+    } catch (error) {
+      console.error("Error fetching search results: ", error);
+    }
+  };
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <main>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                pt: 8,
+                pb: 6,
+              }}
+            >
+              <Container maxWidth="sm">
+                <Typography
+                  component="h1"
+                  variant="h3"
+                  align="center"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  Search for a movie or TV show title
+                </Typography>
+                <Stack
+                  sx={{ pt: 4 }}
+                  direction="column"
+                  spacing={2}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <TextField
+                    label="Enter a title"
+                    variant="outlined"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                  />
+                  <Button variant="contained" onClick={handleSearch}>
+                    Search
+                  </Button>
+                </Stack>
+              </Container>
+            </Box>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            {showResults &&
+              searchResults.map((result, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4}>
                   <Card
                     sx={{
                       height: "100%",
@@ -147,47 +105,30 @@ const Homepage = () => {
                         // 16:9
                         pt: "56.25%",
                       }}
-                      image="https://source.unsplash.com/random?wallpapers"
+                      image={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                      alt={result.title}
                     />
                     <CardContent sx={{ flexGrow: 1 }}>
                       <Typography gutterBottom variant="h5" component="h2">
-                        Heading
+                        <Link
+                          to={`/movie-links/${result.id}/${result.title}`}
+                          style={{ textDecoration: "none" }}
+                        >
+                          {result.title}
+                        </Link>
                       </Typography>
-                      <Typography>
-                        This is a media card. You can use this section to
-                        describe the content.
-                      </Typography>
+                      <Typography>Synopsis: {result.overview}</Typography>
                     </CardContent>
-                    <CardActions>
-                      <Button size="small">View</Button>
-                      <Button size="small">Edit</Button>
-                    </CardActions>
                   </Card>
                 </Grid>
               ))}
-            </Grid>
-          </Container>
-        </main>
-        {/* Footer */}
-        <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-          <Typography variant="h6" align="center" gutterBottom>
-            Footer
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            color="text.secondary"
-            component="p"
-          >
-            Something here to give the footer a purpose!
-          </Typography>
-        </Box>
-        {/* End footer */}
-      </ThemeProvider>
-
-      // <>
-      //   <p>Sample text</p>
-      // </>
-    );
-  }
+          </Grid>
+        </Container>
+      </main>
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        {/* Your existing footer */}
+      </Box>
+    </ThemeProvider>
+  );
+};
 export default Homepage;
